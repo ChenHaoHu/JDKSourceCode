c8be6abc44744448c818707244d0848c0da17f10
hcy <775656764@qq.com>
2019-04-03 23:36:51 +0800

    分析HashMap部分源码

---
+++ Main.java
@@ -0,0 +1,30 @@
+
+
+
+
+
+public class Main{
+    public static void main(String[] args) {
+      
+     
+        System.out.println(tableSizeFor(1)); // ---1
+        System.out.println(tableSizeFor(2)); // ---2
+        System.out.println(tableSizeFor(3)); // ---4
+        System.out.println(tableSizeFor(5)); // ---8
+        System.out.println(tableSizeFor(6)); // ---8
+        System.out.println(tableSizeFor(7)); // ---8
+        System.out.println(tableSizeFor(8)); // ---8
+        System.out.println(tableSizeFor(9)); // ---16
+
+    }
+
+    static  int tableSizeFor(int cap) {
+        int n = cap - 1;
+        n |= n >>> 1;
+        n |= n >>> 2;
+        n |= n >>> 4;
+        n |= n >>> 8;
+        n |= n >>> 16;
+        return (n < 0) ? 1 : (n >= 64) ? 64 : n + 1;
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # JDKSourceCode
-ONLY IN MY OPINION !!! JDK Source Code
+
+
+
+`个人学习记录，他人无学习价值，只有简单本人能看懂的注释`
+
+
+
+> 进度
+* 分析HashMap部分源码
