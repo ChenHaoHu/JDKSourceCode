5a52f015ee37be50148052a9ece788e246747162
hcy <775656764@qq.com>
2019-04-11 19:34:25 +0800

    add Integer

---
+++ JDK 1.8/test/Integer/Main.java
@@ -0,0 +1,18 @@
+package Integer;
+
+public class Main{  
+ 
+    public static void main(String[] args) {
+        Integer i = new Integer(100);
+        System.out.println(i);
+        System.out.println(i.SIZE);  //32 
+        System.out.println(i.toString());
+        System.out.println(Integer.toUnsignedString(1000,20));
+        System.out.println(Integer.toHexString(1000));
+        System.out.println(Integer.toHexString(-1000));
+        System.out.println(Integer.toString(1000,16));
+        System.out.println(i.hashCode());  //重点
+        System.out.println(Integer.decode("02132312")); //解码 自行
+
+    }
+}
